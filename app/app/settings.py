43c7136e1ddcbd0f0cd6d2d46531e185e4e0f05b
Boring-Mind <super.d3280@gmail.com>
2024-09-08 10:42:49 +0200

    feat: installed DRF and accounts app properly

--- app/app/settings.py
+++ app/app/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = "django-insecure-6&%kx^_m5^bir3#!(cxu$4v0gd=*k!mu)u+ek=rqkl4@k_$2=+
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["0.0.0.0", "127.0.0.1", "localhost"]
 
 
 # Application definition
@@ -37,6 +37,10 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
+    # Third-party apps
+    "rest_framework",
+    # Our custom apps
+    "accounts",
 ]
 
 MIDDLEWARE = [
